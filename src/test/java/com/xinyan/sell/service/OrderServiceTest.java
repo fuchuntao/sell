e4b480f7022d75ffd3f438520cbeaa498c78f642
fct201417395 <3066356466@qq.com>
2018-11-18 11:32:23 +0800

    对订单OrderServiceTest测试类进行了修改

--- src/test/java/com/xinyan/sell/service/OrderServiceTest.java
+++ src/test/java/com/xinyan/sell/service/OrderServiceTest.java
@@ -1,15 +1,21 @@
 package com.xinyan.sell.service;
 
+import com.xinyan.sell.dto.OrderDTO;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OrderServiceTest {
+
+    @Autowired
+    private OrderService orderService;
 
     @Test
     public void createOrder() {
@@ -39,5 +45,14 @@ public class OrderServiceTest {
     public void paid() {
 
 
+    }
+
+    //==================================卖家订单测试=====================
+
+    @Test
+    public void listTest(){
+        PageRequest pageRequest = new PageRequest(0,2);
+        Page<OrderDTO> orderDTOPage = orderService.list(pageRequest);
+        Assert.assertNotNull(orderDTOPage);
     }
 }
