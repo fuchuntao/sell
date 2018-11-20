2f8ebc0bd454ce197c8093bcfa5f45bba4bd9c2c
fct201417395 <3066356466@qq.com>
2018-11-20 20:59:38 +0800

    增加订单测试单元

--- src/test/java/com/xinyan/sell/service/OrderServiceTest.java
+++ src/test/java/com/xinyan/sell/service/OrderServiceTest.java
@@ -23,21 +23,34 @@ public class OrderServiceTest {
     @Test
     public void createOrder() {
 
-
     }
 
+    /**
+     * 测试买家订单列表
+     */
     @Test
     public void findList() {
+        PageRequest pageRequest = new PageRequest(0, 5);
+        String buyOpenId = "qwer456";
+        Page<OrderDTO> orderDTOPage = orderService.findList(buyOpenId, pageRequest);
+        Assert.assertNotEquals(0, orderDTOPage.getSize());
 
 
     }
 
+    /**
+     * 测试订单详情
+     */
     @Test
     public void findOne() {
-
+        OrderDTO orderDTO = orderService.findOne("1b08039906a54e699d43aaec8764bb2e");
+        Assert.assertNotNull(orderDTO);
 
     }
 
+    /**
+     * 测试取消订单
+     */
     @Test
     public void cancelTest() {
         OrderDTO orderDTO = orderService.findOne("123");
@@ -45,9 +58,11 @@ public class OrderServiceTest {
         Assert.assertNotNull(cancel);
     }
 
+    /**
+     * 测试支付订单
+     */
     @Test
     public void paid() {
-
 
     }
 
